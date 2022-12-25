e21df5e43284d02f4244dbf202492913a4f71acf
Noertri <trinuruns@gmail.com>
2022-12-25 21:14:09 +0700

    method POST dengan query

--- CRUD.py
+++ CRUD.py
@@ -32,14 +32,6 @@ class SqliteDB:
     def column_names(self):
         return self.cursor.description
 
-    def execute(self, sql, parameters):
-        try:
-            self.cursor.execute(__sql=sql, __parameters=parameters)
-            return self.cursor
-        except Exception as e:
-            logger.error(f"{e}", exc_info=True)
-            return None
-
     def commit(self):
         try:
             self.connection.commit()
@@ -51,45 +43,3 @@ class SqliteDB:
             self.connection.close()
         except Exception as e:
             logger.error(f"{e}", exc_info=True)
-
-    # def create(self, tablename, columns):
-    #     sql = """CREATE TABLE IF NOT EXISTS {0} ({1});""".format(tablename, columns)
-    #
-    #     try:
-    #         self.curr.execute(sql)
-    #         self.conn.commit()
-    #     except Exception as e:
-    #         logger.error(f"{e}", exc_info=True)
-    #
-    # def insert(self, tablename, **records):
-    #     sql = "INSERT INTO {0} ({1}) VALUES ({2});".format(tablename, ", ".join(records.keys()), ", ".join(["?" for _ in range(len(records))]))
-    #     self.curr.execute(sql, [v for v in records.values()])
-    #     self.conn.commit()
-    #     return True
-
-    # def check_duplicate(self, tablename, record):
-    #     sql = "SELECT COUNT(*) FROM {0} WHERE judul=? AND url=?;".format(tablename)
-    #     self.curr.execute(sql, (record["judul"], record["url"]))
-    #     count = self.curr.fetchone()
-    #
-    #     if count[0] == 0:
-    #         return True
-    #     else:
-    #         return False
-
-    # def query_all(self, tablename):
-    #     sql = "SELECT * FROM {0}".format(tablename)
-    #     try:
-    #         self.curr.execute(sql)
-    #         return self.curr.fetchall()
-    #     except Exception as err:
-    #         logger.error(f"{err}", exc_info=True)
-    #         return None
-    #
-    # def close(self):
-    #     try:
-    #         self.conn.close()
-    #     except sqlite3.Error as sqerr:
-    #         logging.error(f"{sqerr}", exc_info=True)
-    #     except Exception as err:
-    #         logger.error(f"{err}", exc_info=True)
--- index.py
+++ index.py
@@ -1,20 +1,29 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, g
 from CRUD import SqliteDB
 
 app = Flask(__name__)
+TABLE = "mahasiswa"
+
+
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 
 @app.route("/mahasiswa", methods=["GET", "POST"])
 def get_mahasiswa():
     db = SqliteDB(database="database_mahasiswa.db")
     if request.method == "GET":
-        all_mhs = db.query_all(tablename="mahasiswa")
+        db.cursor.execute("SELECT * FROM {0}".format(TABLE))
+        all_mhs = db.cursor.fetchall()
         response = make_response(all_mhs)
         response.status_code = 200
     elif request.method == "POST":
-        new_mhs = request.get_json()
+        record = request.args
+        sql2 = "INSERT INTO {0} ({1}) VALUES ({2});".format(TABLE, ", ".join(record.keys()), ", ".join(["?" for _ in range(len(record))]))
         try:
-            db.insert("mahasiswa", **new_mhs)
+            db.cursor.execute(sql2, [v for v in record.values()])
+            db.commit()
             response = make_response("<h1>Success!!!</h1>")
             response.status_code = 201
         except Exception as e:
