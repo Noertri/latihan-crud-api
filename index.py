3f82519b69d52f60968fdc5d9d994d0e4edbed37
Noertri <trinuruns@gmail.com>
2022-12-31 14:50:11 +0700

    tes dan debug

--- index.py
+++ index.py
@@ -3,17 +3,16 @@ from flask import Flask, request, make_response, jsonify
 from models import Mahasiswa
 
 app = Flask(__name__)
-TABLE = "mahasiswa"
 
 
 @app.route("/mahasiswa", methods=("GET",))
 def get_mahasiswa():
-    db = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
+    mhs = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
     args = request.args
 
     if len(args) == 0:
         try:
-            all_mhs = db.query_all()
+            all_mhs = mhs.query_all()
             response = make_response(jsonify(all_mhs))
             response.status_code = 200
         except Exception as e:
@@ -38,19 +37,19 @@ def get_mahasiswa():
 
 @app.route("/mahasiswa", methods=("POST",))
 def insert_mahasiswa():
-    db = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
+    mhs = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
     args = request.args
 
     if all(key.strip() in ("nama", "nim", "jurusan") for key in args.keys()):
         try:
-            db.insert(record=args)
+            mhs.insert(record=args)
             response = make_response({
                 "pesan"      : "Sukses!!!",
                 "kode status": 201
             })
             response.status_code = 201
         except Exception as e:
-            db.cursor.close()
+            mhs.cursor.close()
             response = make_response({
                 "pesan"      : f"Ada masalah di server!!!. {e}",
                 "kode status": 500,
@@ -72,14 +71,14 @@ def insert_mahasiswa():
 
 @app.route("/mahasiswa", methods=("PUT",))
 def update_mahasiswa():
-    db = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
+    mhs = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
     args = request.args
 
     if any(key.strip() in ("id", "nama", "nim", "jurusan") for key in args.keys()):
         try:
             _id = args["id"]
             new_records = dict([(k.strip(), v) for k, v in args.items() if k != "id"])
-            db.update(new_records=new_records, _id=int(_id))
+            mhs.update(new_records=new_records, _id=int(_id))
             response = make_response({
                 "pesan"      : "Sukses!!!",
                 "kode status": 200
@@ -107,12 +106,12 @@ def update_mahasiswa():
 
 @app.route("/mahasiswa", methods=("DELETE",))
 def delete_mahasiswa_by_id():
-    db = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
+    mhs = Mahasiswa(database="database_mahasiswa.db", table="mahasiswa")
     args = request.args
 
     if all(key.strip() in ("id",) for key in args.keys()):
         try:
-            db.delete_by_id(_id=int(args["id"].strip()))
+            mhs.delete_by_id(_id=int(args["id"].strip()))
             response = make_response({
                 "pesan"      : "Sukses!!!",
                 "kode status": 200
